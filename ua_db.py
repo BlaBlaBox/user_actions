54ed0f8918057b770ea58ce656f9939d76744910
Ahmed Yasin KUL <ahmedyasinkul@hotmail.com>
2018-12-16 01:38:19 +0300

    Files renamed and some improvements on code.

--- ua_db.py
+++ ua_db.py
@@ -1,7 +1,6 @@
-from flask import Flask
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-from datetime import date, datetime
-from config import app
+from ua_config import app
 
 db = SQLAlchemy(app)
 
@@ -16,8 +15,6 @@ class Person(db.Model):
 
 
 class User(db.Model):
-    def __init__(self):
-        self.__dict__ = self
 
     person_id = db.Column(db.Integer, db.ForeignKey('person.person_id'))
     user_id = db.Column(db.Integer, primary_key=True)
@@ -28,12 +25,21 @@ class User(db.Model):
     is_active = db.Column(db.Boolean, default=True)
     is_admin = db.Column(db.Boolean, default=False)
 
+    def __init__(self, **kwargs):
+        self.__dict__ = self
+        super().__init__(**kwargs)
 
-def signUp(name, surname, gender, dob, username, pass_hash, email):
+
+def add_person(name, surname, gender, dob):
+
     new_person = Person(firstname=name, surname=surname, gender=gender, dob=dob)
     db.session.add(new_person)
     db.session.commit()
+    return new_person
 
+
+def signUp(name, surname, gender, dob, username, pass_hash, email):
+    new_person = add_person(name, surname, gender, dob)
     p_id = new_person.person_id
 
     new_user = User(person_id=p_id, username=username, pass_hash=pass_hash, mail=email)
@@ -42,13 +48,13 @@ def signUp(name, surname, gender, dob, username, pass_hash, email):
     return new_user
 
 
-
 def getPasshashFromEmail(email):
     return User.query.filter_by(mail=email).first().pass_hash
 
 
 def checkUsername(username):
     return User.query.filter_by(username=username).first()
+
 
 def checkMail(email):
     return User.query.filter_by(mail=email).first()
@@ -79,8 +85,10 @@ def getPassHash(uname_mail, is_mail):
         return User.query.filter_by(mail=uname_mail).first().pass_hash
     return User.query.filter_by(username=uname_mail).first().pass_hash
 
+
 def getAllUsers():
     return User.query.all()
+
 
 def getUser(user_id):
     return User.query.filter_by(user_id=user_id).first()
@@ -88,7 +96,7 @@ def getUser(user_id):
 
 def changeActiveState(user_id):
     user_obj = User.query.filter_by(user_id=user_id).first()
-    user_obj.is_active = not (user_obj.is_active)
+    user_obj.is_active = not user_obj.is_active
     db.session.commit()
     return user_obj.is_active
 
--- ua_ms.py
+++ ua_ms.py
@@ -1,32 +1,40 @@
-from flask import Flask, jsonify, request, abort
-from passlib.hash import pbkdf2_sha256 as hasher
-from datetime import datetime
+from flask import jsonify, request, abort
 from flask_httpauth import HTTPBasicAuth
-from login import signUp, updateUser, changeActiveState, getPassHash, getAllUsers, getUser, checkMail, checkUsername
-from datetime import date, datetime
-from config import app
+from passlib.hash import pbkdf2_sha256 as hasher
+from ua_db import signUp, updateUser, changeActiveState, getPassHash, getAllUsers, getUser, checkMail, checkUsername
+from ua_config import app
+
 
 auth = HTTPBasicAuth()
 
-@app.errorhandler(404)
-def not_found(error):
-    return jsonify({'error': 'Not found'}), 404
 
 @app.errorhandler(400)
-def not_found(error):
+def bad_request():
     return jsonify({'error': 'Your request doesn\'t contain JSON'}), 400
 
 @auth.error_handler
 def unauthorized():
-    return jsonify({'error': 'Unauthorized access'}), 403
+    return jsonify({'error': 'Unauthorized access'}), 401
+
+@app.errorhandler(403)
+def forbidden():
+    return jsonify({'error': 'Forbidden!'}), 403
+
+@app.errorhandler(404)
+def not_found():
+    return jsonify({'error': 'Not found'}), 404
+
+@app.errorhandler(500)
+def internal_server_error():
+    return jsonify({'error' : 'Internal server error'}), 500
 
 
 @app.route('/user/login', methods=['POST'])
 # @auth.login_required
 def login():
     if not request.json:
-        return jsonify({ 'error' : 'Your request is not JSON' }), 400
-   
+        return abort(400)
+
     uname_mail = request.json['uname_mail']
 
     is_mail = False
@@ -35,41 +43,42 @@ def login():
 
     if hasher.verify(request.json['password'], getPassHash(uname_mail, is_mail)):
         return jsonify({'result': 'Success', 'id': uname_mail}), 200 # Password matches
-    else:
-        return jsonify({'result': 'Wrong password or email'}), 400 # Password does not match
+
+    return jsonify({'result': 'Wrong password or email'}), 400 # Password does not match
 
 @app.route('/user/register', methods=['POST'])
 # @auth.login_required
 def register():
     if not request.json:
-        return jsonify({ 'error' : 'Your request is not JSON' }), 400
+        return abort(400)
 
     name = request.json['name']
     surname = request.json['surname']
     gender = request.json['gender']
     dob = request.json['dob']
     username = request.json['username']
-    pass_hash = request.json['password']
+    password = request.json['password']
     email = request.json['email']
 
     if not checkMail(email):
-        return jsonify({'result': 'Email is taken'}), 400 
+        return jsonify({'result': 'Email is taken'}), 400
 
     if not checkUsername(username):
-        return jsonify({'result': 'Username is taken'}), 400 
+        return jsonify({'result': 'Username is taken'}), 400
 
+    pass_hash = hasher.hash(password)
     new_user = signUp(name, surname, gender, dob, username, pass_hash, email)
 
     if not new_user:
-        return jsonify({'result': 'Database is down'}), 500 
-    
-    return jsonify({'result': 'Success', 'id': new_user.user_id}), 200 
-    
+        return abort(500)
+
+    return jsonify({'result': 'Success', 'id': new_user.user_id}), 200
+
 @app.route('/user/update', methods=['POST'])
 @auth.login_required
 def user_update():
     if not request.json:
-        return jsonify({ 'error' : 'Your request is not JSON' }), 400
+        return abort(400)
 
     user_account_id = request.json['id']
     name = request.json['name']
@@ -81,17 +90,18 @@ def user_update():
     email = request.json['email']
 
     if checkMail(email) is not None:
-        return jsonify({'result': 'Email is taken'}), 400 
+        return jsonify({'result': 'Email is taken'}), 400
 
     if checkUsername(username) is not None:
-        return jsonify({'result': 'Username is taken'}), 400 
+        return jsonify({'result': 'Username is taken'}), 400
 
-    updated_user = updateUser(user_account_id, name, surname, gender, dob, username, pass_hash)
+    pass_hash = hasher.hash(password)
+    updated_user = updateUser(user_account_id, name, surname, gender, dob, username, pass_hash, email)
     if not updated_user:
-        return jsonify({'result': 'Database is down'}), 500 
-    
-    return jsonify({'result': 'Success', 'id' : updated_user.user_id}), 200 
-   
+        return abort(500)
+
+    return jsonify({'result': 'Success', 'id' : updated_user.user_id}), 200
+
 # Get all users
 @app.route('/user/get', methods=['GET'])
 #@auth.login_required
@@ -104,11 +114,10 @@ def user_get_all():
         user_dict = user_obj.__dict__
         user_dict.pop('_sa_instance_state')
         users_json.append(user_dict)
-    
+
     if users_json:
         return jsonify({'result': 'Success', 'users' : users_json}), 200
-    else:
-        return jsonify({'result': 'Database is down'}), 500 
+    return abort(500)
 
 # Get spesific user
 @app.route('/user/get/<int:user_id>', methods=['GET'])
@@ -130,13 +139,13 @@ def user_get(user_id):
 def user_activate():
     is_admin = request.authorization
     if not is_admin:
-        return jsonify({'result': 'You are not an admin'}), 401 
+        return abort(401)
 
     user_account_id = request.json['id']
 
     db_result = changeActiveState(user_account_id)
     if db_result is None:
-        return jsonify({'result': 'Database is down'}), 500
+        return abort(500)
 
     return jsonify({'result': 'Success', 'is_active' : db_result}), 200
 
@@ -153,4 +162,4 @@ def verify_password(username, password):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000) 
+    app.run(debug=True, port=8000)
